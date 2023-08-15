a6ae3f3da8c5ccb4275f6f41da6f2186bb7cb89b
mortonvoice <142324923+mortonvoice@users.noreply.github.com>
2023-08-15 15:55:44 -0400

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,14 @@
-const hidden_on_mobile = Array.from(document.getElementsByClassName("hidden-on-mobile"));
-const menu_wrapper = document.getElementsByClassName("menu-wrapper")[0];
+const hidden_on_mobile = Array.from(document.querySelectorAll(".hidden-on-mobile"));
+const menu_wrapper = document.querySelectorAll(".menu-wrapper")[0];
+const english_form = document.querySelector(".english-form")
+const spanish_form = document.querySelector(".spanish-form")
 
 function toggle_menu(){
     menu_wrapper.classList.toggle("change");
     hidden_on_mobile.forEach((element) => element.classList.toggle("change"));
+}
+
+function toggle_iframe(show, hide){
+    show.classList.toggle("hidden")
+    hide.classList.toggle("hidden")
 }
