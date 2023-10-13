795f546fcab9887936eeccc2a1d274c5c586f775
mortonvoice <142324923+mortonvoice@users.noreply.github.com>
2023-10-12 20:43:59 -0400

    Add files via upload

--- javascript/loader.js
+++ javascript/loader.js
@@ -2,11 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const targetElement = document.querySelector(".center-column")
     const storedInnerHTML = localStorage.getItem("innerHTMLData")
 
+    targetElement.innerHTML = "<h1>Something went wrong displaying this piece, please try again later</h1>"
+
     if (targetElement && storedInnerHTML){
         targetElement.innerHTML = storedInnerHTML
         document.querySelector(".see-more").remove()
-    }
-    else{
-        targetElement.innerHTML = "<h1>Something went wrong displaying this piece, please try again later</h1>"
     }
 })
--- javascript/translate.js
+++ javascript/translate.js
@@ -2,7 +2,7 @@ window.gtranslateSettings =
 {
     "default_language": "en",
     "native_language_names": true,
-    "languages": ["en", "es", "zh-TW"],
+    "languages": ["en", "es", "zh-TW", "ru"],
     "wrapper_selector": ".gtranslate_wrapper",
     "flag_size": 24,
     "switcher_open_direction": "top" 
