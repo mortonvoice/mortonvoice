d30915c31d56a7edbfa2f1b9f9969a872aeedd3f
mortonvoice <142324923+mortonvoice@users.noreply.github.com>
2023-11-13 18:01:55 -0500

    Add files via upload

--- javascript/translate.js
+++ javascript/translate.js
@@ -3,6 +3,7 @@ window.gtranslateSettings =
     "default_language": "en",
     "native_language_names": true,
     "languages": ["en", "es", "zh-TW", "ru"],
+    "detect_browser_language": true,
     "wrapper_selector": ".gtranslate_wrapper",
     "flag_size": 24,
     "switcher_open_direction": "top" 
